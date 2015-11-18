465aa3123c481060f808847e5883e03d6ae8d1b1
Erick Castillo de la Garza <ecastillo550@gmail.com>
2015-11-18 16:31:03 -0600

    esbozo de menu

--- main.java
+++ main.java
@@ -2,9 +2,32 @@ import java.io.*;
 import java.text.*;
 
 class Santaanita {
-	public static void main(String arg[]) {
-		String hola = Lectura.readString();
-		System.out.println(hola);
+	public static void main(String args[]) {
+		System.out.println("Bienvenido al sistema de administracion de Santa anita");
+		char userInput;//Variable dedicada a tener el input del usuario
+
+		do { //ciclo de ejecucion del programa
+			System.out.println("escriba 'm' para ver el menu o 'q' para salir");
+			userInput = Lectura.readChar();
+
+			switch (userInput) {
+				case 'm':
+					getMenu();
+					break;
+
+				case 'q':
+					System.out.println("Hasta luego");
+					break;
+
+				default:
+					getMenu();
+					break;
+			}
+		} while (userInput != 'q');
+	}
+
+	public static void getMenu() {
+		System.out.println("run");
 	}
 }
 
