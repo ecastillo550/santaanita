8ed0953cae77b7d3e2d6370ab9f3c9eb9d0a1fab
Erick Castillo de la Garza <ecastillo550@gmail.com>
2015-11-18 16:57:24 -0600

    carga por de datos dummy

--- main.java
+++ main.java
@@ -4,7 +4,8 @@ import java.text.*;
 class Santaanita {
 	public static void main(String args[]) {
 		System.out.println("Bienvenido al sistema de administracion de Santa anita");
-		char userInput;//Variable dedicada a tener el input del usuario
+		char userInput; //Variable dedicada a tener el input del usuario
+		String database[][] = new String[20][13]; //estructura de datos con la infromacion de las casas (12 meses y el apellido)
 
 		do { //ciclo de ejecucion del programa
 			System.out.println("escriba 'm' para ver el menu o 'q' para salir");
@@ -19,6 +20,18 @@ class Santaanita {
 					System.out.println("Hasta luego");
 					break;
 
+				case 'a':
+					System.out.println("Alta de info");
+					break;
+
+				case 'l':
+					cargarDatosDummy(database);
+					break;
+
+				case '1':
+					reporteAnual(database);
+					break;
+
 				default:
 					getMenu();
 					break;
@@ -27,7 +40,30 @@ class Santaanita {
 	}
 
 	public static void getMenu() {
-		System.out.println("run");
+		System.out.println("Dar de alta informacion: a");
+		System.out.println("Reporte anual: 1");
+	}
+
+	public static void reporteAnual(String database[][]) {
+		for (int casa = 0; casa < 20 ; casa++) {
+			for (int mes = 0; mes <= 12 ; mes++) {
+				System.out.println(database[casa][mes]);
+			}
+		}
+	}
+
+	public static void cargarDatosDummy(String database[][]) {
+		database[0][0] = "familia";
+
+		for (int casa = 0; casa < 20 ; casa++) {
+			for (int mes = 0; mes <= 12 ; mes++) {
+				if (mes == 0) {
+					database[casa][mes] = "familia " + casa;
+				} else {
+					database[casa][mes] = "100";
+				}
+			}
+		}
 	}
 }
 
