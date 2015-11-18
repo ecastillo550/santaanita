b4aa4cf57ceef248dc950bf342dafcaabb104c14
Erick Castillo de la Garza <ecastillo550@gmail.com>
2015-11-18 17:20:36 -0600

    despliegue de datos por familia

--- main.java
+++ main.java
@@ -6,6 +6,7 @@ class Santaanita {
 		System.out.println("Bienvenido al sistema de administracion de Santa anita");
 		char userInput; //Variable dedicada a tener el input del usuario
 		String database[][] = new String[20][13]; //estructura de datos con la infromacion de las casas (12 meses y el apellido)
+		String mesesString[] = new String[] {"Enero", "Febero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 
 		do { //ciclo de ejecucion del programa
 			System.out.println("escriba 'm' para ver el menu o 'q' para salir");
@@ -13,7 +14,7 @@ class Santaanita {
 
 			switch (userInput) {
 				case 'm':
-					getMenu();
+					System.out.println(getMenu());
 					break;
 
 				case 'q':
@@ -29,25 +30,48 @@ class Santaanita {
 					break;
 
 				case '1':
-					reporteAnual(database);
+					reporteAnual(database, mesesString);
+					break;
+
+				case '2':
+					System.out.println("escribe el numero de casa que desea ver:");
+					reporteFamilia(database, Lectura.readInt(), mesesString);
 					break;
 
 				default:
-					getMenu();
+					System.out.println(getMenu());
 					break;
 			}
 		} while (userInput != 'q');
 	}
 
-	public static void getMenu() {
-		System.out.println("Dar de alta informacion: a");
-		System.out.println("Reporte anual: 1");
+	public static String getMenu() {
+		String menu;
+
+		menu = "Dar de alta informacion: a\n";
+		menu += "Reporte anual: 1\n";
+
+		return menu;
 	}
 
-	public static void reporteAnual(String database[][]) {
+	public static void reporteAnual(String database[][], String mesesString[]) {
 		for (int casa = 0; casa < 20 ; casa++) {
 			for (int mes = 0; mes <= 12 ; mes++) {
-				System.out.println(database[casa][mes]);
+				if (mes == 0) {
+					System.out.println("\nFamilia: " + database[casa][mes]);
+				} else {
+					System.out.println(mesesString[mes-1] + "\t \t$" + database[casa][mes]);
+				}
+			}
+		}
+	}
+
+	public static void reporteFamilia(String database[][], int casa, String mesesString[]) {
+		for (int mes = 0; mes <= 12 ; mes++) {
+			if (mes == 0) {
+				System.out.println("\nFamilia: " + database[casa][mes]);
+			} else {
+				System.out.println(mesesString[mes-1] + "\t \t$" + database[casa][mes]);
 			}
 		}
 	}
@@ -60,10 +84,25 @@ class Santaanita {
 				if (mes == 0) {
 					database[casa][mes] = "familia " + casa;
 				} else {
-					database[casa][mes] = "100";
+					database[casa][mes] = "0";
 				}
 			}
 		}
 	}
+
+	// public static void cargarMeses(String mesesString[]) {
+	// 	mesesString[0] = "Enero";
+	// 	mesesString[1] = "Febrero";
+	// 	mesesString[2] = "Marzo";
+	// 	mesesString[3] = "Enero";
+	// 	mesesString[4] = "Enero";
+	// 	mesesString[5] = "Enero";
+	// 	mesesString[6] = "Enero";
+	// 	mesesString[7] = "Enero";
+	// 	mesesString[8] = "Enero";
+	// 	mesesString[9] = "Enero";
+	// 	mesesString[10] = "Enero";
+	// 	mesesString[11] = "Enero";
+	// }
 }
 
