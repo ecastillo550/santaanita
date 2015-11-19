ee7a60dbe23d80c3195e85edc98bae8a6546941e
Erick Castillo de la Garza <ecastillo550@gmail.com>
2015-11-19 00:07:12 -0600

    Contribucion de Oscar

--- main.java
+++ main.java
@@ -131,5 +131,54 @@ class Santaanita {
 			}
 		}
 	}
+
+	public static void montoAnio(String database[][]) {
+		int acum=0;
+		for (int i = 0; i < 20 ; i++) {
+			for (int j = 1; j <= 12 ; j++) {
+				acum+=Double.parseDouble(database[i][j]);
+			}
+		}
+		System.out.println("\nEl monto total recaudado del anio es $"+acum);
+	}
+
+	public static void montoMes(String database[][]) {
+		int acum=0;
+		System.out.println("Ingrese el mes");
+		int m=Lectura.readInt();
+		for (int i = 0; i < 20 ; i++) {
+			acum+=Double.parseDouble(database[i][m]);
+		}
+		System.out.println("\nEl monto total recaudado del mes "+m+" es $"+acum);
+	}
+
+
+	public static void deudaMontoAnio(String database[][], int casa, int mes) {
+		double acum=0;
+		double deuda=0;
+		for(casa=0; casa<20; casa++){
+			for(mes=1;mes<13;mes++) {
+				if(Double.parseDouble(database[casa][mes])==0);
+				else if(Double.parseDouble(database[casa][mes])<9500)
+					deuda+=9500-Double.parseDouble(database[casa][mes]);
+				acum+=deuda;
+			}
+			casa++;
+		}
+		System.out.println("La deuda total del anio es de: $" +deuda);
+	}
+
+
+	public static void deudaMontoMes(String database[][]) {
+		double deuda=0;
+		System.out.println("Ingrese el mes");
+		int m=Lectura.readInt();
+		for(int i=0;i<20;i++) {
+			if(Double.parseDouble(database[i][m])==0);
+			else if(Double.parseDouble(database[i][m])<9500)
+				deuda+=9500-Double.parseDouble(database[i][m]);
+		}
+		System.out.println("La deuda total del mes " +m +" es de: $" +deuda);
+	}
 }
 
